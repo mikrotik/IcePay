f8cedec58e19d8fceba762a3c01796bfef8b5220
Ahmet GOUDENOGLU <ahmet.gudenoglu@gmail.com>
2017-10-30 12:22:21 +0100

    Api

--- application/libraries/gateway/Api.php
+++ application/libraries/gateway/Api.php
@@ -23,14 +23,26 @@ class Api extends Rest
         $this->setCode($code);
         $this->setContentType($contentType);
 
-        $output['Result'] = $code;
-        $output['Message'] = $this->get_rest_status_message();
-        $output['TrackingMemberCode'] = 'Test 1111-2222-3333-4444';
-        $output['TransactionId'] = UUID::trxid(8);
-        $output['TransactionGuid'] = UUID::v5('1546058f-5a25-4334-85ae-e68f2a44bbaf',UUID::trxid(8));
-        $output['TransactionDateTime'] = $this->_objDateTime->format(DateTime::ISO8601);
+        if ($hasError == false && $data['ResultState'] == true) {
+            $output['Result'] = $code;
+            $output['Message'] = $this->get_rest_status_message();
+            $output['TrackingMemberCode'] = 'Test 1111-2222-3333-4444';
+            $output['TransactionId'] = UUID::trxid(8);
+            $output['TransactionGuid'] = UUID::v5('1546058f-5a25-4334-85ae-e68f2a44bbaf', UUID::trxid(8));
+            $output['TransactionDateTime'] = $this->_objDateTime->format(DateTime::ISO8601);
 
-        $this->response($this->$contentType($output), $status);
+            $this->response($this->$contentType($output), $status);
+        } else {
+            $error['Result'] = $code;
+            $error['Message'] = $this->get_rest_status_message();
+            $error['Reason'] = 'Error message from processor';
+            $error['TrackingMemberCode'] = 'Test 5555-6666-7777-8888';
+            $error['TransactionId'] = UUID::trxid(8);
+            $error['TransactionGuid'] = UUID::v5('1546058f-5a25-4334-85ae-e68f2a44bbaf', UUID::trxid(8));
+            $error['TransactionDateTime'] = $this->_objDateTime->format(DateTime::ISO8601);
+
+            $this->response($this->$contentType($error), $status);
+        }
 
     }
 
