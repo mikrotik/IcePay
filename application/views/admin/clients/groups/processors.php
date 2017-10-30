b474845c92a01fdabfdd8d3abba1837c4cd79d84
Ahmet GOUDENOGLU <ahmet.gudenoglu@gmail.com>
2017-10-30 14:26:03 +0100

    Client Processors

--- application/views/admin/clients/groups/processors.php
+++ application/views/admin/clients/groups/processors.php
@@ -2,6 +2,6 @@
     <h4 class="no-mtop bold"><?php echo _l('client_processors_tab'); ?></h4>
     <hr />
     <?php if(has_permission('processors','','create')){ ?>
-        <a href="<?php echo admin_url('processors/customer_processor?customer_id='.$client->userid); ?>" class="btn btn-info mbot25<?php if($client->active == 0){echo ' disabled';} ?>"><?php echo _l('create_new_processor'); ?></a>
+        <a href="<?php echo admin_url('processors/processor?customer_id='.$client->userid); ?>" class="btn btn-info mbot25<?php if($client->active == 0){echo ' disabled';} ?>"><?php echo _l('create_new_processor'); ?></a>
     <?php } ?>
 <?php } ?>
